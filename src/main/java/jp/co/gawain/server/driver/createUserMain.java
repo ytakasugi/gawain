d7316d5e55ae1d187b935f30978594851d451a5e
ytakasugi <sh7.tibi0129@gmail.com>
2023-08-01 22:35:22 +0900

    change file

--- src/main/java/jp/co/gawain/server/driver/createUserMain.java
+++ src/main/java/jp/co/gawain/server/driver/createUserMain.java
@@ -1,13 +1,21 @@
 package jp.co.gawain.server.driver;
 
-import java.util.List;
+import java.sql.Connection;
 import java.util.ArrayList;
+import java.util.List;
 
-import jp.co.gawain.server.api.UserApi;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import jp.co.gawain.server.constans.GawainMessageConstants;
+import jp.co.gawain.server.dao.UserDao;
 import jp.co.gawain.server.dto.UserDto;
+import jp.co.gawain.server.util.DatabaseManager;
 
 public class createUserMain {
+    private static Logger logger = LoggerFactory.getLogger(createUserMain.class);
     public static void main(String[] args) {
+        Connection connection = null;
         List<UserDto> newUserList = new ArrayList<UserDto>();
 
         for (int i = 0; i < 50; i++) {
@@ -23,9 +31,23 @@ public class createUserMain {
         }
 
         int size = newUserList.size();
-        
-        for (int i = 0; i < size; i++) {
-            UserApi.create(newUserList.get(i));
+
+        try {
+            connection = DatabaseManager.getConnection();
+            UserDao dao = new UserDao(connection);
+
+            for (int i = 0; i < size; i++) {
+                logger.info(GawainMessageConstants.APPLICATION_INFO_MESSAGE_003);
+                dao.create(newUserList.get(i));
+                logger.info(GawainMessageConstants.APPLICATION_INFO_MESSAGE_004);
+                DatabaseManager.commit(connection);
+            }
+        } catch (Exception e) {
+            logger.error(GawainMessageConstants.APPLICATION_ERROR_MESSAGE_001, e);
+            DatabaseManager.rollback(connection);
+        } finally {
+            DatabaseManager.close(connection);
+            DatabaseManager.cloeseDatasSource();
         }
     }
 }
